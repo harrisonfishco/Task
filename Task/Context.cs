5428483410308242898b4a9409b9aa0e74c22d7d
Harrisonfishco <harrisonfishco@gmail.com>
2024-05-13 22:13:42 -0700

    7 - Added Lookup Tables

--- Task/Context.cs
+++ Task/Context.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Task.Login;
+using Task.Lookup;
 
 namespace Task
 {
@@ -10,6 +11,8 @@ namespace Task
 
         public DbSet<TaskUser> TaskUsers { get; set; }
         public DbSet<TaskUserSession> TaskUserSessions { get; set; }
+        public DbSet<TaskLookup> TaskLookups { get; set; }
+        public DbSet<TaskLookupValue> TaskLookupValues { get; set; }
 
         public Context(DbContextOptions<Context> options)
             : base(options)
@@ -21,6 +24,18 @@ namespace Task
         {
             modelBuilder.Entity<TaskUser>()
                 .HasAlternateKey(e => e.Username);
+
+            modelBuilder.Entity<TaskUser>()
+                .HasData(new TaskUser
+                {
+                    UserGu = Guid.NewGuid(),
+                    Username = "admin",
+                    Email = "admin@webadmin.com",
+                    Password = "$2a$10$XsoM977PKMnbd1zzIEpJH.UoHjKb5VuFqF/Sq1es/xtYyijshEItG", //password
+                    AddTimestamp = DateTime.Now,
+                });
+
+            TaskLookup.OnModelCreating(modelBuilder);
         }
 
         public async Task<bool> VerifyUser(string username, string password)
@@ -50,7 +65,7 @@ namespace Task
 
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public Guid UserGU { get; set; }
+        public Guid UserGu { get; set; }
 
         [Required]
         [StringLength(20)]
@@ -73,13 +88,15 @@ namespace Task
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public Guid UserSessionGU { get; set; }
-        [Required]
-        public Guid UserGU { get; set; }
+        public Guid UserSessionGu { get; set; }
         public DateTime? Expires { get; set; }
         public DateTime? AddTimestamp { get; set; }
         public DateTime? UpdateTimestamp { get; set; }
 
+        [Required]
+        public Guid UserGu { get; set; }
+
+        [Required]
         public TaskUser? TaskUser { get; set; }
     }
 }
