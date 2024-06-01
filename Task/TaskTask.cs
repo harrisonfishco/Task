888e4247007b13a6948708194f15826cf16a3518
Harrison Fishco <harrisonfishco@gmail.com>
2024-06-01 09:22:42 -0700

    30 - Removed 22

--- Task/TaskTask.cs
+++ Task/TaskTask.cs
@@ -2,7 +2,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
-using Task.Models;
 using System.ComponentModel;
 
 namespace Task
@@ -15,7 +14,6 @@ namespace Task
             Description = string.Empty;
             Status = string.Empty;
             Opener = new TaskUser();
-            Project = new TaskProject();
         }
 
         public static void OnModelCreating(ModelBuilder modelBuilder)
@@ -26,12 +24,6 @@ namespace Task
                 .HasForeignKey(t => t.OpenerGu)
                 .HasPrincipalKey(u => u.UserGu)
                 .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<TaskTask>()
-                .HasOne(t => t.Project)
-                .WithMany(p => p.Tasks)
-                .HasForeignKey(t => t.ProjectGu)
-                .HasPrincipalKey(p => p.ProjectGu);
         }
 
         [Key]
@@ -61,12 +53,5 @@ namespace Task
         public TaskUser Opener { get; set; }
 
         public ICollection<TaskActivity>? Activities { get; set; }
-
-        [Required]
-        public Guid ProjectGu { get; set; }
-
-        [Required]
-        [ForeignKey(nameof(ProjectGu))]
-        public TaskProject Project { get; set; }
     }
 }
