a54d1e74ae66ae6bea836fe685d6dabe8f7ece67
Harrisonfishco <harrisonfishco@gmail.com>
2024-06-28 23:04:26 -0700

    22 - Added forwarding for QA

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Task.Components;
 using Task.Notifications;
@@ -38,6 +39,11 @@ namespace Task
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
 
             app.UseHttpsRedirection();
 
