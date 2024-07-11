881a20adb3a8a7b2dc6fd69c885b60f4f5c710c7
aozturk1 <aozturk1@asu.edu>
2024-07-10 20:23:27 -0700

    45 - setting up new service for authentication adding service to the configuration

---
+++ Task/Authentication.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.EntityFrameworkCore;
+
+namespace Task
+{
+    public class Authentication
+    {
+        public async void IsAuthenticated(NavigationManager navigationManager, Session session, IDbContextFactory<Context> ContextFactory)
+        {
+            Guid sessionId = await session.GetSessionId();
+
+            using (Context ctx = ContextFactory.CreateDbContext())
+            {
+                TaskUserSession? sess = await ctx.TaskUserSessions.Where(s => s.UserSessionGu == sessionId).FirstOrDefaultAsync();
+
+                if (TypeCheck.NotEmpty(sess))
+                {
+
+                    navigationManager.NavigateTo("/");
+                }
+            }
+        }
+    }
+}
